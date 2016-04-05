700f699ae69a5a8d1993a00759e32c76c7adb68d
modesty <modestyz@hotmail.com>
2016-04-05 14:26:32 -0700

    v0.0.6: change rx to peerDependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const SfAction = require('./dist/sf.action');
-const SfStore = require('./dist/sf.store');
-
-module.exports = {Action: SfAction, Store: SfStore};
+module.exports = {
+	Action: require('./dist/sf.action'),
+	Store: require('./dist/sf.store')
+};
